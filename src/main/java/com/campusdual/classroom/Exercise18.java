0fe3fe9f8edbd59e96731faa2880a6315cf439f0
Aldair-GL <alejandro555azpeitia@gmail.com>
2025-05-26 09:12:10 +0200

    Exercise 18 solved.

--- src/main/java/com/campusdual/classroom/Exercise18.java
+++ src/main/java/com/campusdual/classroom/Exercise18.java
@@ -1,8 +1,28 @@
 package com.campusdual.classroom;
 
+
 public class Exercise18 {
 
 	public static void main(String[] args) {
+		showInlineArray(createAndInitializeArray(5));
+	}
 
+	public static int[] createAndInitializeArray(int index) {
+		int num = 1;
+		int[] arraySample = new int[index];
+		for (int i = 0; i < index; i++) {
+			arraySample[i] = num;
+			num++;
+		}
+		return arraySample;
+	}
+
+	public static void showInlineArray(int[] array) {
+		for (int i = 0; i < array.length; i++) {
+			System.out.print(array[i]);
+			if (i < array.length - 1) {
+				System.out.print(" ");
+			}
+		}
 	}
 }
